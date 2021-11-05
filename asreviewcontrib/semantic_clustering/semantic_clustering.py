5cdc7050fa024c80027185cf80bc83e1dfb8739b
Jelle <j.j.teijema@gmail.com>
2021-11-05 23:55:35 +0100

    Move all functionality to one single pipeline (#17)

---
+++ asreviewcontrib/semantic_clustering/semantic_clustering.py
@@ -0,0 +1,119 @@
+# Copyright 2021 The ASReview Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# imports
+import os
+from tqdm import tqdm
+from dim_reduct import run_pca
+from dim_reduct import t_sne
+from clustering import run_KMeans
+from asreview.data import ASReviewData
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+from transformers import logging
+import matplotlib.pyplot as plt
+import seaborn as sns
+logging.set_verbosity_error()
+sns.set()
+tqdm.pandas()
+
+
+def SemanticClustering(asreview_data_object):
+
+    # load data
+    print("Loading data...")
+    data = load_data(asreview_data_object)
+
+    # cut data for testing
+    data = data.iloc[:300, :]
+
+    # load scibert transformer
+    print("Loading scibert transformer...")
+    transformer = 'allenai/scibert_scivocab_uncased'
+
+    # load transformer and tokenizer
+    print("Loading tokenizer and model...")
+    tokenizer = AutoTokenizer.from_pretrained(transformer)
+    model = AutoModel.from_pretrained(transformer)
+
+    # tokenize abstracts and add to data
+    print("Tokenizing abstracts...")
+    data['tokenized'] = data['abstract'].progress_apply(
+        lambda x: tokenizer.encode_plus(
+            x,
+            add_special_tokens=False,
+            truncation=True,
+            max_length=512,
+            padding='max_length',
+            return_tensors='pt'))
+
+    # generate embeddings and format correctly
+    print("Generating embeddings...")
+    data['embeddings'] = data['tokenized'].progress_apply(lambda x: model(
+        **x,
+        output_hidden_states=False)[-1].detach().numpy().squeeze())
+
+    # from here on the data is not directly attached to the dataframe anymore,
+    # as a result of legacy code. This will be fixed in a future PR.
+
+    # run pca
+    print("Running PCA...")
+    pca = run_pca(data['embeddings'].tolist(), n_components=.98)
+
+    # run t-sne
+    print("Running t-SNE...")
+    tsne = t_sne(pca, n_iter=1000)
+
+    # run k-means
+    print("Running k-means...")
+    labels = run_KMeans(tsne, 10, 10)
+
+    # visualize clusters
+    print("Visualizing clusters...")
+    tsne_data = [tsne[:, 0], tsne[:, 1]]
+    visualize_clusters(tsne_data, labels)
+
+
+def visualize_clusters(data, labels):
+    fig, ax = plt.subplots()
+    ax.set_title("semantic clustering")
+    ax.set_xlabel("t-SNE Component 1")
+    ax.set_ylabel("t-SNE Component 2")
+
+    x = data[0]
+    y = data[1]
+
+    # Do actual plotting and save image
+    ax.scatter(x, y, c=labels, cmap="Set3")
+    if not os.path.exists("img"):
+        os.makedirs("img")
+    filename = "clusters.png"
+    img_path = os.path.join("img", filename)
+    fig.savefig(img_path)
+
+
+def load_data(asreview_data_object):
+
+    # extract title and abstract, drop empty abstracts and reset index
+    data = asreview_data_object.df[['title', 'abstract']].copy()
+    data['abstract'] = data['abstract'].replace('', np.nan, inplace=False)
+    data.dropna(subset=['abstract'], inplace=True)
+    data = data.reset_index(drop=True)
+
+    return data
+
+
+if __name__ == "__main__":
+    filepath = "https://raw.githubusercontent.com/asreview/systematic-review-datasets/master/datasets/van_de_Schoot_2017/output/van_de_Schoot_2017.csv"
+    SemanticClustering(ASReviewData.from_file(filepath))
