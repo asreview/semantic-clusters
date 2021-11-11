7922b381572ba818827217a602494ebce9c99ac7
Teijema <j.j.teijema@gmail.com>
2021-11-11 13:08:44 +0100

    Solve Flake8 issues with main.py

--- asreviewcontrib/semantic_clustering/__init__.py
+++
@@ -1,2 +0,0 @@
-from .semantic_clustering import SemanticClustering
-from .interactive import run_app
--- asreviewcontrib/semantic_clustering/main.py
+++ asreviewcontrib/semantic_clustering/main.py
@@ -34,7 +34,7 @@ def main(argv):
         elif opt in ("-f", "--filepath"):
             filepath = arg
         elif opt in ("-t", "--testfile"):
-            filepath = "https://raw.githubusercontent.com/asreview/systematic-review-datasets/master/datasets/van_de_Schoot_2017/output/van_de_Schoot_2017.csv"
+            filepath = "https://raw.githubusercontent.com/asreview/systematic-review-datasets/master/datasets/van_de_Schoot_2017/output/van_de_Schoot_2017.csv" #noqa: E501
         elif opt in ("-a", "--app"):
             run_app()
             sys.exit(1)
@@ -50,10 +50,10 @@ def main(argv):
 
     SemanticClustering(ASReviewData.from_file(filepath))
 
+
 class SemClusEntryPoint(BaseEntryPoint):
     description = "Semantic clustering tools for ASReview."
     extension_name = "asreview-semantic-clustering"
-
 
     def __init__(self, args):
         super().__init__()
